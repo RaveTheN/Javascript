e15dcd6c8e0ffd4e54526989aa6e09a39810298a
RaveTheN <peppereres@live.it>
2022-12-14 10:28:39 +0100

    esercizio svolto

---
+++ exercise-1.js
@@ -0,0 +1,20 @@
+const person1 = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 25,
+};
+
+const person2 = person1;
+
+person2.firstName = "Simon";
+
+// for (key in person1){
+//   person2 [key] = person1 [key]
+// }
+
+// Modify the property `firstName` of the `person2` in "Simon".
+//Changing the name in person2 modifies also the name in person1 as both the variables head to the same memory
+//unit in which the data is allocated.
+
+console.log(person1);
+console.log(person2);
