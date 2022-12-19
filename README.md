aa90334ca7e81357c93fecefdd557da80decc659
RaveTheN <peppereres@live.it>
2022-12-19 15:46:17 +0100

    svolto

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Javascript
+Starting from the previous exercise, are you able to print the value of helloName, after 1 second?
---
+++ exercise.js
@@ -0,0 +1,14 @@
+let helloName;
+
+function printName () {
+    this.helloName = "Hello John";
+    function inner () {
+        setTimeout(() =>{
+            console.log(this.helloName)
+        }, 1000)
+        
+    }
+    return inner;
+}
+
+printName()()
