e52718aab9b5ed8cec2d2974945d0863a3fc861d
RaveTheN <peppereres@live.it>
2022-12-21 13:01:39 +0100

    esercizio 14

--- exercise1.js
+++ exercise1.js
@@ -1,51 +1,63 @@
-Create a function called uncompletedNotes that returns only not completed todos.
+//Create a function called uncompletedNotes that returns only not completed todos.
+
+function uncompletedNotes (notes){
+let array = [];
+
+notes.forEach ((el) => 
+array.push(el.todos.filter((elmnt) => elmnt.done == false )))
+
+return array;
+
+}
 
 const notes = [
-  {
-    id: 1,
-    description: "Workout program",
-    todos: [
-      {
+{
+id: 1,
+description: "Workout program",
+todos: [
+{
         id: 1,
         name: "Push ups - 10 x 3",
         done: false,
-      },
-      {
+},
+{
         id: 2,
         name: "Abdominals - 20 x 3",
         done: true,
-      },
-      {
+},
+{
         id: 3,
         name: "Tapis Roulant - 15min",
         done: true,
-      },
-    ],
-  },
-  {
-    id: 2,
-    description: "Front-end Roadmap",
-    todos: [
-      {
+},
+],
+},
+{
+id: 2,
+description: "Front-end Roadmap",
+todos: [
+{
         id: 1,
         name: "Learn HTML",
         done: true,
-      },
-      {
+},
+{
         id: 2,
         name: "Learn CSS",
         done: true,
-      },
-      {
+},
+{
         id: 3,
         name: "Learn JavaScript",
         done: true,
-      },
-      {
+},
+{
         id: 4,
         name: "Learn Angular",
         done: false,
-      },
-    ],
-  },
+},
+],
+},
 ];
+
+console.log(uncompletedNotes(notes));
