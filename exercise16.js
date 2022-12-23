318851a06d7474f9ca46221ae84c20c0543a84b5
RaveTheN <peppereres@live.it>
2022-12-23 18:35:54 +0100

    ersercizio 16

---
+++ exercise16.js
@@ -0,0 +1,11 @@
+//Define a class called Person that takes in two arguments (firstName and lastName) in the constructor.
+
+class Person {
+    constructor (firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+const developer = new Person('Mario', 'Rossi');
+console.log(developer.firstName + " " + developer.lastName);
